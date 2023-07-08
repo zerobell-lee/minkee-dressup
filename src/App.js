1f6fbee8f8e61020276076bfaa2665b983b7b4e0
unknown <lee@zerobell.xyz>
2023-07-08 22:54:06 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/scss/style.scss';
+import React, { useState } from 'react';
 
 function App() {
+
+  // ATM you need to manually add new items and update the total values per item 
+  const [dressupState, setDressupState] = useState({
+    hairs: {current: 11, total: 12},
+    clothes: {current: 4, total: 5},
+    weapons: {current: 3, total: 4},
+    accessoriesA: {current: 7, total: 8},
+  });
+
+  function next(item){
+    let next_num = dressupState[item].current + 1
+    // if next_num exceeds total, restart (set current to 0)
+    let new_current = next_num < dressupState[item].total ? next_num : 0
+    updateDressUp(item, new_current)
+  }
+
+  function updateDressUp(item,new_current){
+    setDressupState({
+      ...dressupState,
+      [item]: {
+        current: dressupState[item].current = new_current, 
+        total: dressupState[item].total
+      }
+    })
+  }
+
+  function randomize(){
+    // for each dressup item, generate a random integer and assign it to current 
+    Object.keys(dressupState).map((item) => 
+      updateDressUp(item, Math.floor(Math.random() * Math.floor(dressupState[item].total)))
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>나만의 민킈를 만들어보자!!!</h1>
+      <div id="container">
+        <div id="background">
+          <div id="body"></div>
+          { Object.keys(dressupState).map((item) => 
+                <div id={item} className={item+(dressupState[item].current+1)} key={item}></div>
+              )
+            } 
+        </div>
+      </div>
+
+      <div id="controlPanel">
+      { Object.keys(dressupState).map((item) => 
+          <input type="button" value={"next "+item} key={item} id={"next"+item} onClick={() => next(item)}/>
+        )
+      }
+      </div>
     </div>
   );
 }
